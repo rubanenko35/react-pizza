dee21f8b6e6ec8639bcee66549c5c8f646ce1d6d
OleksandrRubanenko <ICOleksandr.Rubanenko@americanwell.com>
2024-11-12 11:53:03 +0200

    create order

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./ui/Home.jsx";
 import Menu, { loader as menuLoader } from "./features/menu/Menu.jsx";
 import Cart from "./features/cart/Cart.jsx";
 import Order, { loader as orderLoader } from "./features/order/Order.jsx";
-import CreateOrder from "./features/order/CreateOrder.jsx";
+import CreateOrder, { action as createOrderAction} from "./features/order/CreateOrder.jsx";
 import AppLayout from "./ui/AppLayout.jsx";
 import Error from "./ui/Error.jsx";
 
@@ -28,6 +28,7 @@ const router = createBrowserRouter([
       },
       {
         path: '/order/new',
+        action: createOrderAction,
         element: <CreateOrder/>,
       },
       {
