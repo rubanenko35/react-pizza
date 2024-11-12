eba5536d42edadffa6619b04678abcc05e6686dc
OleksandrRubanenko <ICOleksandr.Rubanenko@americanwell.com>
2024-11-12 19:22:12 +0200

    button styling

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
-import {Form, redirect, useActionData, useNavigation} from "react-router-dom";
-import {createOrder} from "../../services/apiRestaurant.js";
+import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
+import { createOrder } from "../../services/apiRestaurant.js";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
-    str
+    str,
   );
 
 const fakeCart = [
@@ -34,7 +34,7 @@ const fakeCart = [
 
 function CreateOrder() {
   const navigation = useNavigation();
-  const isSubmitting = navigation.state === 'submitting';
+  const isSubmitting = navigation.state === "submitting";
 
   const formErrors = useActionData();
 
@@ -78,24 +78,30 @@ function CreateOrder() {
         </div>
 
         <div>
-          <input type="hidden" name="cart" value={JSON.stringify(cart)}/>
-          <button disabled={isSubmitting}>{isSubmitting ? "Placing order..." : "Order now"}</button>
+          <input type="hidden" name="cart" value={JSON.stringify(cart)} />
+          <button
+            className="inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed"
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? "Placing order..." : "Order now"}
+          </button>
         </div>
       </Form>
     </div>
   );
 }
 
-
-export async function action({request}) {
+export async function action({ request }) {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
 
+  return null;
+
   const order = {
     ...data,
-    priority: data.priority === 'on',
-    cart: JSON.parse(data.cart)
-  }
+    priority: data.priority === "on",
+    cart: JSON.parse(data.cart),
+  };
 
   const newOrder = await createOrder(order);
 
