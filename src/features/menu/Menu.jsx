23ca694164bc665466fc88409a7b60bf6c8382b7
OleksandrRubanenko <ICOleksandr.Rubanenko@americanwell.com>
2024-11-25 11:30:08 +0200

    menu styles

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,13 +1,17 @@
-import {getMenu} from "../../services/apiRestaurant.js";
-import {useLoaderData} from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant.js";
+import { useLoaderData } from "react-router-dom";
 import MenuItem from "./MenuItem.jsx";
 
 function Menu() {
   const menu = useLoaderData();
 
-  return <ul>
-    {menu.map((pizza) => <MenuItem pizza={pizza} key={pizza.id} /> )}
-  </ul>;
+  return (
+    <ul className="divide-y divide-stone-200 px-2">
+      {menu.map((pizza) => (
+        <MenuItem pizza={pizza} key={pizza.id} />
+      ))}
+    </ul>
+  );
 }
 
 export async function loader() {
